0452981b2b45178a7f1542e73f16a2172409c906
Conor-O-Mahony <conor.omahony@ucdconnect.ie>
2024-05-18 17:39:06 +0100

    Updated to main

--- src/main.py
+++ src/main.py
@@ -34,6 +34,18 @@ class SetUp:
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument('--window-size=1440,900')  
         chrome_options.add_argument('--headless')
+        prefs = {'profile.default_content_setting_values': {'cookies': 1, 'images': 2, 'javascript': 1, 
+                            'plugins': 2, 'popups': 2, 'geolocation': 2, 
+                            'notifications': 2, 'auto_select_certificate': 2, 'fullscreen': 2, 
+                            'mouselock': 2, 'mixed_script': 2, 'media_stream': 2, 
+                            'media_stream_mic': 2, 'media_stream_camera': 2, 'protocol_handlers': 2, 
+                            'ppapi_broker': 2, 'automatic_downloads': 2, 'midi_sysex': 2, 
+                            'push_messaging': 2, 'ssl_cert_decisions': 2, 'metro_switch_to_desktop': 2, 
+                            'protected_media_identifier': 2, 'app_banner': 2, 'site_engagement': 2, 
+                            'durable_storage': 2}}
+        chrome_options.add_experimental_option('prefs', prefs)
+        chrome_options.add_argument("disable-infobars")
+        chrome_options.add_argument("--disable-extensions")
         user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
         chrome_options.add_argument(f'user-agent={user_agent}')
 
